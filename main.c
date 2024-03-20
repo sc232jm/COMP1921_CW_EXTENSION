81c0a659861c7e5f7d082ce5f168245c16052d5f
sc232jm <145260982+sc232jm@users.noreply.github.com>
2024-03-20 14:42:41 +0000

    FINISHEDgit add .!

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 int height;
 int width;
 
+int lastX;
+int lastY;
+
 typedef struct cell {
     int key;
     int xPos;
@@ -30,8 +33,7 @@ typedef struct node {
 
 typedef struct queue {
     int arrSize;
-    int head;
-    int tail;
+    int pointer;
     node *arr[1024];
 } queue;
 
@@ -39,24 +41,22 @@ queue nodeQueue;
 cell **grid;
 node *graph;
 
-void enqueue(node *n) {
-    nodeQueue.arr[nodeQueue.tail] = n;
-    nodeQueue.tail++;
+void push(node *n) {
+    nodeQueue.pointer++;
+    nodeQueue.arr[nodeQueue.pointer] = n;
     nodeQueue.arrSize++;
 }
 
-void dequeue() {
-    nodeQueue.head++;
-    nodeQueue.arrSize--;
+void pop() {
+    nodeQueue.pointer--;
 }
 
-void addNeighbour(int key, int destKey, int weight) {
+void addNeighbour(int key, int destKey) {
     for(int i = 0; i < height*width; i++) {
         if (graph[i].key == key) {
             edge e;
             e.destKey = destKey;
-            e.weight = weight;
-
+            
             graph[i].edges[graph[i].edgeCount] = e; 
             graph[i].edgeCount++;
         }
@@ -67,21 +67,20 @@ void generateGraph() {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j ++ ) {
             int key = grid[i][j].key;
-            int randWeight = (rand() % 5);
-            if (i - 1 >= 0) {
-                addNeighbour(key, grid[i-1][j].key, randWeight);
+            if (i - 2 >= 0) {
+                addNeighbour(key, grid[i-2][j].key);
             }
             
-            if (j - 1 >= 0) {
-                addNeighbour(key, grid[i][j-1].key, randWeight);
+            if (j - 2 >= 0) {
+                addNeighbour(key, grid[i][j-2].key);
             }
 
-            if (i + 1 < height) {
-                addNeighbour(key, grid[i+1][j].key, randWeight);
+            if (i + 2 < height) {
+                addNeighbour(key, grid[i+2][j].key);
             }
             
-            if (j + 1 < width) {
-                addNeighbour(key, grid[i][j+1].key, randWeight);
+            if (j + 2 < width) {
+                addNeighbour(key, grid[i][j+2].key);
             }
         }
             
@@ -133,39 +132,61 @@ cell* selectRand(int x, int y) {
     return &grid[edgeList[selection].xPos][edgeList[selection].yPos];
 };
 
-void flagWalls(int x, int y) {
-    node selectedNode = graph[grid[x][y].key];
-     for (int i = 0; i<selectedNode.edgeCount; i++) {
-        node targetNode = graph[selectedNode.edges[i].destKey];
-        grid[targetNode.xPos][targetNode.yPos].visited = 1;
-    };
+void removeWall(node *rootNode, node *selectedNode) {
+    int deltaX = selectedNode->xPos - rootNode->xPos;
+    int deltaY = selectedNode->yPos - rootNode->yPos;
+
+    cell *targetCell;
+
+    switch(deltaX < 0 ? 0 : deltaY < 0 ? 2 : deltaX > 0 ? 1 : 3) {
+        case 0:
+            targetCell = &grid[rootNode->xPos-1][rootNode->yPos];
+            break;
+        case 1:
+            targetCell = &grid[rootNode->xPos+1][rootNode->yPos];
+            break;
+        case 2:
+            targetCell = &grid[rootNode->xPos][rootNode->yPos-1];
+            break;
+        case 3:
+            targetCell = &grid[rootNode->xPos][rootNode->yPos+1];
+            break;
+    }
+
+    targetCell->visited = 1;
+    if (targetCell->type != 'S') {
+        
+        lastX = targetCell->xPos;
+        lastY = targetCell->yPos;
+       targetCell->type = ' ';
+    }
 }
 
 void pathGeneration() {
     int randX = 1 + (rand() % (height-1));
     int randY = 1 + (rand() % (width-1));
 
-    enqueue(&graph[grid[randX][randY].key]);
+    push(&graph[grid[randX][randY].key]);
     grid[randX][randY].visited = 1;
     grid[randX][randY].type = 'S';
     
-    while(nodeQueue.arrSize > 0) {
+    while(nodeQueue.pointer > 0) {
         cell *selected = NULL;
-        selected = selectRand(nodeQueue.arr[nodeQueue.head]->xPos, nodeQueue.arr[nodeQueue.head]->yPos);
+        selected = selectRand(nodeQueue.arr[nodeQueue.pointer]->xPos, nodeQueue.arr[nodeQueue.pointer]->yPos);
+
         if (selected == NULL) {
-            dequeue();
+            if (grid[nodeQueue.arr[nodeQueue.pointer]->xPos][nodeQueue.arr[nodeQueue.pointer]->yPos].type != 'S') {
+                grid[nodeQueue.arr[nodeQueue.pointer]->xPos][nodeQueue.arr[nodeQueue.pointer]->yPos].type = ' ';
+            }
+            pop();
         } else {
             selected->visited = 1;
-            if (grid[nodeQueue.arr[nodeQueue.head]->xPos][nodeQueue.arr[nodeQueue.head]->yPos].type != 'S') {
-                grid[nodeQueue.arr[nodeQueue.head]->xPos][nodeQueue.arr[nodeQueue.head]->yPos].type = ' ';
-            }
-
-            flagWalls(nodeQueue.arr[nodeQueue.head]->xPos, nodeQueue.arr[nodeQueue.head]->yPos);
-            enqueue(&graph[selected->key]);
+            removeWall(nodeQueue.arr[nodeQueue.pointer], &graph[selected->key]);
+            push(&graph[selected->key]);
         }
     }
 
-    grid[nodeQueue.arr[nodeQueue.head-1]->xPos][nodeQueue.arr[nodeQueue.head-1]->yPos].type = 'E';
+    grid[lastX][lastY].type = 'E';
 };
 
 int main(int argc, char *argv[]) {
